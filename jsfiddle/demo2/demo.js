0c8f447ec79b37c85465183598dca59d94f2c67f
Eric <netkuup@gmail.com>
2017-06-11 19:05:29 +0200

    .

--- jsfiddle/demo2/demo.js
+++ jsfiddle/demo2/demo.js
@@ -1,12 +1,1 @@
-
-window.addEvent('domready', function() {
-    new Request.HTML({
-        url: '/gh/get/response.html/zalun/jsFiddleGithubDemo/tree/master/Demo/',
-        data: {'delay': 1},
-        method: 'post',
-        update: 'demo',
-        onSuccess: function(response) {
-            $('demo').highlight();
-        }
-    }).send();
-})
+NKPopup.start( true );
